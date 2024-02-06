bb73b2930777dfb5785bfe4ed8004a6f65e63a71
BlasterWhite <icarambala@gmail.com>
2024-02-06 20:15:41 +0100

    🗻 Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="home">
+    <h1 class="logo">URL shortener</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.home {
+  text-align: center;
+  padding: 20px;
+  font-size: 1.5em;
+  color: #333;
+}
+</style>
